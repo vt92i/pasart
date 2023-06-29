af7b132058e6036065934dfe24d5d9730c0dc005
fikaa <benidiktusvio@gmail.com>
2023-06-29 09:23:11 +0100

    📦 chore: add Firebase configuration files and project setup 🔧 chore: add .gitignore file to exclude node_modules, dist, and .firebase directory 🔧 chore: add Firebase hosting configuration in firebase.json 🆕 feat: add index.html file with meta tags and basic structure 🆕 feat: add package.json file with project dependencies and scripts 🆕 feat: add postcss.config.js file for Tailwind CSS configuration 🆕 feat: add hero.png and logo.jpeg images to the public/images directory 🆕 feat: add App.tsx file with a basic Solid component structure 🆕 feat: add index.css file with Tailwind CSS imports 🆕 feat: add index.tsx file with Solid app rendering logic 🆕 feat: add logo.svg file for the project logo 🆕 feat: add tailwind.config.js file for Tailwind CSS configuration 🆕 feat: add tsconfig.json file with TypeScript compiler options 🆕 feat: add vite.config.ts file for Vite configuration The changes include adding Firebase configuration files (.firebaserc, firebase.json), project setup files (.gitignore, package.json), HTML file (index.html) with meta tags and basic structure, CSS files (index.css, postcss.config.js) for styling, image files (hero.png, logo.jpeg, logo.svg) for the project, Solid component file (App.tsx) with a basic structure, TypeScript configuration file (tsconfig.json), and Vite configuration file (vite.config.ts). These files are necessary for setting up the project and configuring Firebase hosting.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div class="flex min-h-screen items-center justify-center bg-[url('/images/hero.png')] bg-cover bg-center bg-no-repeat w-full"></div>
+  );
+};
+
+export default App;
